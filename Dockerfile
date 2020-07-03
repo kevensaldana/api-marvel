4c8019a4c58e0f782cefc833ef5621d71bcb36a4
keven <keven.sa17@gmail.com>
2020-07-03 01:06:25 -0500

    feat: debug

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:3.7
 
-RUN pip install fastapi uvicorn httpx dependency-injector
+RUN pip install fastapi uvicorn httpx dependency-injector pydevd-pycharm~=192.6817.19
 
 COPY ./app /app
 
--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from app.api.controllers.character_list_controller import CharacterListController
 
+import pydevd_pycharm
+pydevd_pycharm.settrace('host.docker.internal', port=4200, stdoutToServer=True, stderrToServer=True)
+
 app = FastAPI()
 app.add_api_route("/", CharacterListController.run)
